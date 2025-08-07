58ed55087a19006c6c54b6af9ea6d2afa9e5fefe
jklzz02 <jacopo.lazzaro@gmail.com>
2025-08-07 10:31:50 +0200

    Remove useless import

--- src/services/anime.service.ts
+++ src/services/anime.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpUrlEncodingCodec } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { forkJoin, Observable } from 'rxjs';
 import { Anime } from '../interfaces/anime';
