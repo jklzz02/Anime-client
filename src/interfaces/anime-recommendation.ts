2801174769fb307691d6d5a5f9da52793ce5a9cf
jklzz02 <jacopo.lazzaro.jl@gmail.com>
2025-12-08 17:37:49 +0100

    new recommender service

---
+++ src/interfaces/anime-recommendation.ts
@@ -0,0 +1,15 @@
+export interface AnimeRecommendation {
+  id: number;
+  title: string;
+  score: number;
+  release_year: number;
+  synopsis: string;
+  episodes: number;
+  studio: string;
+  rating: string;
+  type: string;
+  source: string;
+  genres: string;
+  started_airing: string;
+  recommendation_score: number;
+}
