72c88f6ed009ba4afdcb8b88b97cbe6d060da599
Jacopo <121625293+jklzz02@users.noreply.github.com>
2025-12-20 15:37:40 +0100

    Angular update and dependency cleanup

---
+++ src/app/reviews/markdown-editor/markdown-editor.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-markdown-editor',
+  standalone: false,
+  templateUrl: './markdown-editor.component.html',
+  styleUrl: './markdown-editor.component.css'
+})
+export class MarkdownEditorComponent {
+
+}
