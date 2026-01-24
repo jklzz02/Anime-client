8dfa0e7937d7f960d9b23821d3f704d7e9fc869f
jklzz02 <jacopo.lazzaro.jl@gmail.com>
2026-01-24 19:03:51 +0100

    use funny api for error component

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -1,6 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-error',
@@ -11,18 +13,28 @@ import { Router } from '@angular/router';
 export class ErrorComponent implements OnInit {
   statusCode: number = 0;
   message: string | null = null;
+  imageUrl: string = '';
+
+  private statusImageMap: Record<number, string> = {
+    404: 'https://api.waifu.pics/sfw/cringe',
+    401: 'https://api.waifu.pics/sfw/smug',
+    403: 'https://api.waifu.pics/sfw/kick',
+    500: 'https://api.waifu.pics/sfw/cry',
+  };
 
   constructor(
+    private http: HttpClient,
     private router: Router,
     private title: Title,
   ) {
     const navigation = this.router.getCurrentNavigation();
     const state = navigation?.extras?.state;
-
     if (state) {
       const code = Number(state['status']);
       if (!Number.isNaN(code) && code !== 0) {
         this.statusCode = code;
+      } else {
+        this.statusCode = 500;
       }
       this.message = state['message'] || null;
     }
@@ -34,5 +46,11 @@ export class ErrorComponent implements OnInit {
     } else {
       this.title.setTitle('Error ' + this.statusCode);
     }
+
+    this.errorImageUrl().subscribe((data) => (this.imageUrl = data.url));
+  }
+
+  errorImageUrl(): Observable<any> {
+    return this.http.get(this.statusImageMap[this.statusCode]);
   }
 }
