4976e9f2ae70450bbe2cb5e74e08e8c24a87b4cc
jklzz02 <jacopo.lazzaro.jl@gmail.com>
2026-02-07 20:35:51 +0100

    Don't add unnecesary key for other services

--- src/interceptors/header/header.interceptor.ts
+++ src/interceptors/header/header.interceptor.ts
@@ -4,7 +4,10 @@ import { environment } from '../../environments/environment';
 export const headerInterceptor: HttpInterceptorFn = (req, next) => {
   let headers = req.headers;
 
-  if (!headers.has('X-Client-Key')) {
+  if (
+    !headers.has('X-Client-Key') &&
+    req.url.startsWith(environment.anime_api_domain)
+  ) {
     headers = headers.set('X-Client-Key', environment.x_client_key);
   }
 
