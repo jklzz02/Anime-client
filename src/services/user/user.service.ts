13c7b93d8013f211d7cc64de2f9c48a415ae3d7f
Jacopo <121625293+jklzz02@users.noreply.github.com>
2026-02-08 15:11:05 +0100

    add admin related functions to user service

--- src/services/user/user.service.ts
+++ src/services/user/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { User } from '../../interfaces/user';
@@ -13,44 +13,60 @@ export class UserService {
   public currentUser$ = this.currentUserSubject.asObservable();
 
   private BASE: string = environment.anime_api_domain;
-  private headers: HttpHeaders = new HttpHeaders({
-    'X-Client-Key': environment.x_client_key,
-  });
 
   constructor(private http: HttpClient) {}
 
   getCurrentUser(): Observable<User> {
     return this.http
-      .get<User>(`${this.BASE}/user`, {
-        headers: this.headers,
-        withCredentials: true,
-      })
+      .get<User>(`${this.BASE}/user`)
       .pipe(tap((user) => this.currentUserSubject.next(user)));
   }
 
   getFavourites(): Observable<UserFavourite[]> {
-    return this.http.get<UserFavourite[]>(`${this.BASE}/user/favourite`, {
-      headers: this.headers,
-      withCredentials: true,
-    });
+    return this.http.get<UserFavourite[]>(`${this.BASE}/user/favourite`);
   }
 
   addFavourite(animeId: number): Observable<UserFavourite> {
     return this.http.post<UserFavourite>(
       `${this.BASE}/user/favourite`,
       animeId,
-      {
-        headers: this.headers,
-        withCredentials: true,
-      }
     );
   }
 
-  removeFavourite(animeId: number): Observable<void> {
-    return this.http.delete<void>(`${this.BASE}/user/favourite/${animeId}`, {
-      headers: this.headers,
-      withCredentials: true,
+  getUserList(page: number, pageSize: number): Observable<User[]> {
+    return this.http.get<User[]>(`${this.BASE}/users`, {
+      params: {
+        page: page.toString(),
+        pageSize: pageSize.toString(),
+      },
     });
+  }
+
+  removeFavourite(animeId: number): Observable<void> {
+    return this.http.delete<void>(`${this.BASE}/user/favourite/${animeId}`);
+  }
+
+  unbanUser(email: string): Observable<void> {
+    return this.http.post<void>(`${this.BASE}/user/unban`, { email });
+  }
+
+  banUser(email: string): Observable<void>;
+  banUser(email: string, reason: string): Observable<void>;
+  banUser(
+    email: string,
+    reason?: string,
+    expirationDate?: Date,
+  ): Observable<void> {
+    const params: any = { email };
+
+    if (reason) {
+      params.reason = reason;
+    }
+    if (expirationDate) {
+      params.expiration = expirationDate.toUTCString();
+    }
+
+    return this.http.post<void>(`${this.BASE}/user/ban`, { params });
   }
 
   clearUser(): void {
